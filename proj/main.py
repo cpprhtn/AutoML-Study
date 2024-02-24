3818542dff5c64b3a0e245e130cf13a1e2b95bb6
cpprhtn <cpprhtn@naver.com>
2024-02-25 00:36:31 +0900

    Update main.py

--- proj/main.py
+++ proj/main.py
@@ -1,15 +1,18 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-
 import sys
 sys.path.append("/Users/cpprhtn/Documents/GitHub/AutoML-Study/proj")
+
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+
 from make_model import *
 
 app = FastAPI()
 
 class set_Data(BaseModel):
     path: str
+    max_trials: int
+    epochs: int
 
 
 @app.get("/")
@@ -17,8 +20,8 @@ async def root():
     return {"message": "Hello World"}
 
 @app.post("/make_model")
-def makeModel(data_path:set_Data):
-    result = Training_Model(data_path.path)
+def makeModel(data:set_Data):
+    result = Training_Model(data.path, data.max_trials, data.epochs)
     response_content = {"status": "success", "result": result}
 
     return JSONResponse(content=response_content, status_code=200)
